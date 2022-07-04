7dd7213f6dadf478872690c19395b19e62c5fcec
caiooliveirah09 <leelros@hotmail.com>
2022-07-04 18:12:40 -0300

    i solved requirement 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.asyncGetUser();
+  }
+
+  asyncGetUser = async () => {
+    this.setState(
+      { loading: true },
+      async () => {
+        this.setState({ userName: await getUser(), loading: false });
+      },
+    );
+  }
+
   render() {
+    const { userName, loading } = this.state;
+    const { name } = userName;
+    console.log(name);
     const divStyle = { backgroundColor: '#322a31',
       color: 'white',
       fontSize: '35px',
       padding: '15px',
     };
 
+    if (loading) {
+      return <Loading />;
+    }
+
     return (
-      <div style={ divStyle }>
-        <p>TrybeTunes</p>
+      <div data-testid="header-component" style={ divStyle }>
+        <span>TrybeTunes</span>
+        <span data-testid="header-user-name">{name}</span>
       </div>
     );
   }
 }
+
+Header.propTypes = {
+  // userName: PropTypes.string.isRequired,
+};
 
 export default Header;
