c921aa9768a09734d9f6dbb4efbbce11f65b5223
caiooliveirah09 <leelros@hotmail.com>
2022-07-05 20:02:43 -0300

    i solved requirement 5

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      buttonDisabled: true,
+    };
+  }
+
+  validateInput = (event) => {
+    const min = 2;
+    const input = event.target.value;
+    if (input.length >= min) {
+      this.setState({ buttonDisabled: false });
+    } else { this.setState({ buttonDisabled: true }); }
+  }
+
   render() {
-    return <div data-testid="page-search">search</div>;
+    const { buttonDisabled } = this.state;
+    return (
+      <div data-testid="page-search">
+        <form>
+          <label htmlFor="search-artist-input">
+            Artist
+            <textarea
+              data-testid="search-artist-input"
+              cols="10"
+              rows="1"
+              onChange={ this.validateInput }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ buttonDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
