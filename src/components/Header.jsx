1628b6ffe3175e4f75b2ec9d5ee3dc038bc326b7
caiooliveirah09 <leelros@hotmail.com>
2022-07-05 19:38:36 -0300

    i solved requirement 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -41,6 +42,11 @@ class Header extends React.Component {
     return (
       <div data-testid="header-component" style={ divStyle }>
         <span>TrybeTunes</span>
+        <nav>
+          <Link data-testid="link-to-search" to="/search">Search</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+          <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+        </nav>
         <span data-testid="header-user-name">{name}</span>
       </div>
     );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class Search extends React.Component {
   render() {
-    const { logged } = this.props;
-    console.log(logged);
-    if (logged) {
-      return <div data-testid="page-search">search</div>;
-    }
-    return <div data-testid="page-search">permission denied, please login! </div>;
+    return <div data-testid="page-search">search</div>;
   }
 }
-
-Search.propTypes = {
-  logged: PropTypes.bool.isRequired,
-};
 
 export default Search;
