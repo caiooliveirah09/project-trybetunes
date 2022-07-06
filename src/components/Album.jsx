1ea8ef7cf2ed37e21d847297541ec5d67cd63d17
caiooliveirah09 <leelros@hotmail.com>
2022-07-06 11:32:03 -0300

    i solved requirement 7

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import getMusics from '../services/musicsAPI';
+import Loading from './Loading';
+import MusicCard from './MusicCard';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      musics: [],
+      loading: false,
+      artistName: '',
+      collectionName: '',
+    };
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    this.setState({ loading: true }, async () => {
+      const fetchMusics = await getMusics(id);
+      this.setState({ musics: fetchMusics.slice(1),
+        loading: false,
+        artistName: fetchMusics[0].artistName,
+        collectionName: fetchMusics[0].collectionName });
+    });
+  }
+
   render() {
-    return <div data-testid="page-album">album</div>;
+    const { loading, artistName, collectionName, musics } = this.state;
+    if (loading) {
+      return <Loading />;
+    }
+    return (
+      <div data-testid="page-album">
+        <span data-testid="artist-name">{ artistName }</span>
+        <span data-testid="album-name">{ collectionName }</span>
+        {musics.map((music) => (<MusicCard
+          key={ music.previewUrl }
+          trackName={ music.trackName }
+          previewUrl={ music.previewUrl }
+        />))}
+      </div>
+    );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Album;
